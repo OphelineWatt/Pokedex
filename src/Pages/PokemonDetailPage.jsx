6a9bc61729dea5f16270038584840cdab8bf8ca6
Ophéline Wattier <opheliiinee@gmail.com>
2025-06-26 16:56:56 +0200

    affichage details+

--- src/Pages/PokemonDetailPage.jsx
+++ src/Pages/PokemonDetailPage.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import ProgressBar from 'react-bootstrap/ProgressBar';
 import Figure from 'react-bootstrap/Figure';
+import typeColors from '../utils/typeColors';
 
 const PokemonDetailPage = () => {
     const [pokemonDetails, setPokemonDetail] = useState([]);
@@ -28,10 +29,13 @@ const PokemonDetailPage = () => {
     }
 
    
-    const [names, setName] = useState()
-    const [sprite, setSprite] = useState()
+    const [names, setName] = useState();
+    const [sprite, setSprite] = useState();
+    const [types, setType] = useState([]);
+    const [height, setHeight] = useState();
+    const [weight, setweight] = useState();
 
-    const fetchPhoto = async (pokemonC) => {
+    const fetchPhoto = async () => {
         try {
             const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`, {
         
@@ -39,9 +43,12 @@ const PokemonDetailPage = () => {
 
             setSprite(response.data.sprites.other['home'].front_default);
             setName(response.data.name);
-
+            setType(response.data.types);
+            setHeight(response.data.height);
+            setweight(response.data.weight);
             
-            console.log(response.data.types);
+            
+            console.log(response.data);
             
             
 
@@ -59,17 +66,47 @@ const PokemonDetailPage = () => {
   
     
 
-    return (  <Figure className='d-flex flex-column align-items-center justify-content-center'>
+    return (  <Figure className='d-flex flex-column align-items-center justify-content-center gap-3'>
       <Figure.Image
         width={171}
         height={180}
         alt="171x180"
         src={sprite}
       />
-    
-        
         
         <h2 className="pokemon-title">{names}</h2>
+
+        <div className="rowStyle">
+            <p>Taille : {height/10}M</p>
+            <p>Poids: {weight/10}Kg</p>
+        </div>
+
+    
+        
+
+
+        {/* console.log(type.type.name) */}
+            
+<div className="rowStyle">
+  {types.map((type) => (
+    <p
+      key={type.type.name}
+      style={{
+        backgroundColor: typeColors[type.type.name],
+        padding: "5px 15px",
+        borderRadius: "20px",
+        textTransform: "capitalize",
+        margin: "5px"
+      }}
+    >
+      {type.type.name}
+    </p>
+  ))}
+</div>
+
+
+        
+        
 
         <div>
   {pokemonDetails.map((pokemonDetail, index) => (
