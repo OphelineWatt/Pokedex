f9bcad26c1e222970e3ac6f4c8471527a6760511
Ophéline Wattier <opheliiinee@gmail.com>
2025-06-27 11:35:07 +0200

    recherche + pagination

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,49 +1,95 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import PokemonCard from '../Components/PokemonCard';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import PokemonCard from "../Components/PokemonCard";
+import InputGroup from 'react-bootstrap/InputGroup';
 
 const HomePage = () => {
-    const [pokemons, setPokemon] = useState([]);
+  const [pokemons, setPokemon] = useState([]);
+  const [page, setPage] = useState(1);
+  const [search, setSearch] = useState("");
 
+  const fetchPokemon = async () => {
+    try {
+    if (search ==="") {
+      const response = await axios.get(
+        `https://pokeapi.co/api/v2/pokemon?limit=20&offset=${page}`,
+        {}
+      );
+      setPokemon(response.data.results);
+      
 
-    const fetchPokemon = async () => {
-        try {
-            const response = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=20`, {
-            });
-            setPokemon(response.data.results);
-            // console.log(response);
-            
-            
+       } else {
+        //Recherche des pokemon
+        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${search}`);
+        //pokemons est un tableau  d'objet
+       setPokemon([{ name: response.data.name }]);
 
-        } catch (error) {
-            console.error("Error fetching movies:", error);
+       
+        
         }
+    } catch (error) {
+      console.error("Error fetching movies:", error);
     }
+  };
 
-    
+  useEffect(() => {
+    fetchPokemon();
+  }, [page, search]);
 
-    useEffect(() => {
-        fetchPokemon();
-    }, []);
+  const handleSearchChange = (e) => {
+    setSearch(e.target.value);
+    setPage(1);
+  };
 
+  return (
+    <div className="d-flex flex-column align-items-center justify-content-center gap-3">
+      <h1 className="pokemon-title">Pokedex</h1>
 
-    return <div className='d-flex flex-column align-items-center justify-content-center'>
-        <h1 className="pokemon-title">Pokedex</h1>
+      <input
+        type="texte"
+        className='barre form-control col-4'
+        placeholder="Rechercher un pokemon"
+        value={search}
+        onChange={handleSearchChange}
+      />
 
+      <div className="d-flex justify-content-between align-items-center col-8">
+        <button
+          className="btn btn-outline-primary"
+          onClick={() => setPage(page - 20)}
+        >
+          ◀ Précédent​
+        </button>
+        <button
+          className="btn btn-outline-primary"
+          onClick={() => setPage(page + 20)}
+        >
+          Suivant ▶​
+        </button>
+      </div>
 
-    <input
-        type='texte'
-        className='barre form-control col-8'
-        placeholder='Rechercher un pokemon'
-    />
+      <div className="d-flex flex-wrap justify-content-around align-content-center gap-5 col-8">
+        {pokemons.map((pokemon) => {
+          return <PokemonCard key={pokemon.name} pokemonC={pokemon} />;
+        })}
+      </div>
 
+      <div className="d-flex justify-content-between align-items-center col-8">
+        <button
+          className="btn btn-outline-primary"
+          onClick={() => setPage(page - 20)}
+        >
+          ◀ Précédent​
+        </button>
+        <button
+          className="btn btn-outline-primary"
+          onClick={() => setPage(page + 20)}
+        >
+          Suivant ▶​
+        </button>
+      </div>
+    </div>
+  );
+};
 
-        <div className='d-flex flex-wrap justify-content-around align-content-center gap-5 col-8'>
-            {pokemons.map((pokemon) => {
-                return <PokemonCard key={pokemon.name} pokemonC={pokemon} />
-            })}
-        </div>
-    </div>;
-}
- 
 export default HomePage;
--- src/Pages/PokemonDetailPage.jsx
+++ src/Pages/PokemonDetailPage.jsx
@@ -1,127 +1,104 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import ProgressBar from 'react-bootstrap/ProgressBar';
-import Figure from 'react-bootstrap/Figure';
-import typeColors from '../utils/typeColors';
+import ProgressBar from "react-bootstrap/ProgressBar";
+import Figure from "react-bootstrap/Figure";
+import typeColors from "../utils/typeColors";
 
 const PokemonDetailPage = () => {
-    const [pokemonDetails, setPokemonDetail] = useState([]);
-    const {name} =useParams();
+  const [pokemonDetails, setPokemonDetail] = useState([]);
+  const { name } = useParams();
 
+  //Pour récuperer les details de mes pokemon
+  const fetchDetail = async (pokemon) => {
+    try {
+      const response = await axios.get(
+        `https://pokeapi.co/api/v2/pokemon/${name}`,
+        {}
+      );
 
-//Pour récuperer les details de mes pokemon
-    const fetchDetail = async (pokemon) => {
-        try {
-            const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`, {
-        
-            });
+      setPokemonDetail(response.data.stats);
 
-            setPokemonDetail(response.data.stats)
-            
-            // console.log(response.data.stats);
-            
-            
-
-        } catch (error) {
-            console.error("Error fetching detail pokemon:", error);
-        }
+      // console.log(response.data.stats);
+    } catch (error) {
+      console.error("Error fetching detail pokemon:", error);
     }
+  };
 
-   
-    const [names, setName] = useState();
-    const [sprite, setSprite] = useState();
-    const [types, setType] = useState([]);
-    const [height, setHeight] = useState();
-    const [weight, setweight] = useState();
+  const [names, setName] = useState();
+  const [sprite, setSprite] = useState();
+  const [types, setType] = useState([]);
+  const [height, setHeight] = useState();
+  const [weight, setweight] = useState();
 
-    const fetchPhoto = async () => {
-        try {
-            const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`, {
-        
-            });
+  const fetchPhoto = async () => {
+    try {
+      const response = await axios.get(
+        `https://pokeapi.co/api/v2/pokemon/${name}`,
+        {}
+      );
 
-            setSprite(response.data.sprites.other['home'].front_default);
-            setName(response.data.name);
-            setType(response.data.types);
-            setHeight(response.data.height);
-            setweight(response.data.weight);
-            
-            
-            console.log(response.data);
-            
-            
+      setSprite(response.data.sprites.other["home"].front_default);
+      setName(response.data.name);
+      setType(response.data.types);
+      setHeight(response.data.height);
+      setweight(response.data.weight);
 
-        } catch (error) {
+      console.log(response.data);
+    } catch (error) {
+      console.error("Error fetching  pokemon:", error);
+    }
+  };
+  useEffect(() => {
+    fetchDetail();
+    fetchPhoto();
+  }, []);
 
-            console.error("Error fetching  pokemon:", error);
-        }
+  return (
+    <Figure className="d-flex flex-column align-items-center justify-content-center gap-3">
+      <Figure.Image width={171} height={180} alt="171x180" src={sprite} />
 
-}
-    useEffect(()=>{
-        fetchDetail();
-        fetchPhoto();
-    }, []);
+      <h2 className="pokemon-title">{names}</h2>
 
-  
-    
+      <div className="rowStyle">
+        <p>Taille : {height / 10}M</p>
+        <p>Poids: {weight / 10}Kg</p>
+      </div>
 
-    return (  <Figure className='d-flex flex-column align-items-center justify-content-center gap-3'>
-      <Figure.Image
-        width={171}
-        height={180}
-        alt="171x180"
-        src={sprite}
-      />
-        
-        <h2 className="pokemon-title">{names}</h2>
+      {/* console.log(type.type.name) */}
 
-        <div className="rowStyle">
-            <p>Taille : {height/10}M</p>
-            <p>Poids: {weight/10}Kg</p>
-        </div>
+      <div className="rowStyle">
+        {types.map((type) => (
+          <p
+            key={type.type.name}
+            style={{
+              backgroundColor: typeColors[type.type.name],
+              padding: "5px 15px",
+              borderRadius: "20px",
+              textTransform: "capitalize",
+              margin: "5px",
+            }}
+          >
+            {type.type.name}
+          </p>
+        ))}
+      </div>
 
-    
-        
+      <div>
+        {pokemonDetails.map((pokemonDetail, index) => (
+          <div key={pokemonDetail.stat.name || index}>
+            <p>{pokemonDetail.stat.name}</p>
 
+            <ProgressBar
+              max={200}
+              now={pokemonDetail.base_stat}
+              label={pokemonDetail.base_stat}
+            />
+          </div>
+        ))}
+      </div>
+    </Figure>
+  );
+};
 
-        {/* console.log(type.type.name) */}
-            
-<div className="rowStyle">
-  {types.map((type) => (
-    <p
-      key={type.type.name}
-      style={{
-        backgroundColor: typeColors[type.type.name],
-        padding: "5px 15px",
-        borderRadius: "20px",
-        textTransform: "capitalize",
-        margin: "5px"
-      }}
-    >
-      {type.type.name}
-    </p>
-  ))}
-</div>
-
-
-        
-        
-
-        <div>
-  {pokemonDetails.map((pokemonDetail, index) => (
-    <div key={pokemonDetail.stat.name || index}>
-      <p>{pokemonDetail.stat.name}</p>
-
-      <ProgressBar 
-      max={200} 
-      now={pokemonDetail.base_stat}
-      label={pokemonDetail.base_stat}
-      /> 
-    </div>
-  ))}
-</div>
-    </Figure> );
-}
- 
 export default PokemonDetailPage;
